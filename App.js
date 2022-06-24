b21954196d956892a78b85dc7728752abf702f22
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2022-06-25 00:24:01 +0530

    update

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [todoItem, setTodoItem] = useState("");
+  const [todoLst, setTodoLst] = useState([]);
+
+  const textHandler = (e) => {
+    setTodoItem(e);
+    console.log(todoItem);
+  };
+
+  const addTodoList = () => {
+    setTodoLst([...todoLst, todoItem]);
+    setTodoItem("")
+  };
+  console.log(todoLst);
+
+  const disableButton=todoItem.length===0
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{fontWeight:"Bold",}}>Write Your Todos</View>
+      <View>
+        <TextInput
+          placeholder="enter a todo"
+          style={styles.textInput}
+          onChangeText={textHandler}
+          value={todoItem}
+        />
+
+        <Button disabled={disableButton} title="Add todo" onPress={addTodoList} />
+      </View>
+      <View>
+        {todoLst.map((element, i) => {
+          return (
+            <View style={styles.todo}>
+              {" "}
+              <Text key={i}>{element}</Text>
+            </View>
+          );
+        })}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  textInput: {
+    padding: 10,
+    borderColor: "black",
+    marginBottom: 10,
+    borderWidth: 1,
+  },
+  todo: {
+    padding: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    backgroundColor: "yellow",
+    color: "white",
+    margin: 10,
   },
 });
